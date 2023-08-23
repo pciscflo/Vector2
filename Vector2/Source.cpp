bf8cfa819181ea5d183edc7d683a8ebfddc2f9c2
Alumnos <falvarezponce@gmail.com>
2023-08-23 14:42:16 -0500

    Add project files.

---
+++ Vector2/Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <conio.h>
+#include <algorithm>
+using namespace std;
+
+/*
+* Desarrollar un programa en C++ tal que permita a la Empresa "Multiautos" gestionar
+  la venta de autos, los datos de los autos que debe registrar son: codigo, marca,
+  modelo, precio de costo y stock.
+  El precio final es igual al precio de costo más el IGV menos el descuento.
+* El descuento consiste en que si el stock es maytor que 100 entonces se aplica un
+  descuento del 20% sobre el precio de costo.
+* Imprimir un reporte donde muestre los datos del auto y su precio final.
+*/
